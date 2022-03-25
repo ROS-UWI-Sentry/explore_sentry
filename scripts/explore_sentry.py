375f2e42e91e1300f92a4359f6c95c7eae707945
uwi-sentry-agx <uwi@uwi.com>
2022-03-25 17:35:55 -0400

    updates

--- scripts/explore_sentry.py
+++ scripts/explore_sentry.py
@@ -66,6 +66,9 @@ odom_wheels_callback_data = Odometry()
 #for zed2 imu readings:
 zed2_imu_callback_data = Imu()
 
+#for publishing base velocity values in twist format
+base_velocity = Twist()
+
 #this callback function gets called whenever the occupancy grid is updated
 def callback_occupancy_grid(data):
     global occupancy_grid_callback_data
@@ -130,12 +133,8 @@ def callback(data):
 
 def listener():
  
-    # In ROS, nodes are uniquely named. If two nodes with the same
-    # name are launched, the previous one is kicked off. The
-    # anonymous=True flag means that rospy will choose a unique
-    # name for our 'listener' node so that multiple listeners can
-    # run simultaneously.
-    #check for message on Topic
+
+    
 
     global occupancy_grid_callback_data, wheel_encoder_velocity_callback_data
     global odom_wheels_callback_data, zed2_imu_callback_data
@@ -144,12 +143,12 @@ def listener():
 
     #this call creates a subscriber and defines message type and which topic it publishes to
     #whenever a message is received it calls the callback function
-    rospy.Subscriber('/timer_control_topic', String, callback)
+    
 
     #occupancy grid subscriber from rtabmap:
     rospy.Subscriber('/map', OccupancyGrid, callback_occupancy_grid)
-    #wheel velocity subscriber:
-    rospy.Subscriber('/wheel_vel_vector', Num, callback_wheel_encoder)
+    #actual wheel velocity from encoders subscriber:
+    rospy.Subscriber('/encoder_rps', Num, callback_wheel_encoder)
     #wheel odometry subscriber:
     rospy.Subscriber("odom_wheels", Odometry, callback_odom_wheels)
     
@@ -161,24 +160,29 @@ def listener():
     rate = rospy.Rate(1) #1Hz
     
 
-    #while ROS is not shutdown via terminal etc, if the conditions are met, publish counting from 0 to 100
-    i = 0
-    t = 30
+    #while ROS is not shutdown via terminal etc, run this in a loop at a rate of "rate Hz"
     while not rospy.is_shutdown():
    
-        #Pull variables from callbacks
-        #occupancy_grid:
+        #Pull variables from callbacks so that they don't change during calculation 
+
+        #occupancy_grid information:
         map_resolution=occupancy_grid_callback_data.info.resolution
         map_width=occupancy_grid_callback_data.info.width
         map_height=occupancy_grid_callback_data.info.height
+        #occupancy grid data:
+        map_cells=occupancy_grid_callback_data.data
+        #pose of map origin:
+        #x,y,z position of map origin:
         map_pose_x=occupancy_grid_callback_data.info.origin.position.x
         map_pose_y=occupancy_grid_callback_data.info.origin.position.y
         map_pose_z=occupancy_grid_callback_data.info.origin.position.z
+        #x,y,z,w quaternion of map origin:
         map_ori_x=occupancy_grid_callback_data.info.origin.orientation.x
         map_ori_y=occupancy_grid_callback_data.info.origin.orientation.y
         map_ori_z=occupancy_grid_callback_data.info.origin.orientation.z
         map_ori_w=occupancy_grid_callback_data.info.origin.orientation.w
-        map_cells=occupancy_grid_callback_data.data
+        #end of pose of map origin
+        
         
         #wheel encoder velocity:
         wheel_encoder_velocity_left=wheel_encoder_velocity_callback_data[0]
@@ -205,26 +209,44 @@ def listener():
         imu_lin_vel_y=zed2_imu_callback_data.linear_acceleration.y
         imu_lin_vel_z=zed2_imu_callback_data.linear_acceleration.z
 
-        print(imu_lin_vel_z)
+        #print(imu_lin_vel_z)
 
+        #################################
+        #insert navigation algorithm here
+        #################################
 
+        #if using any loops consider adding "while not rospy.is_shutdown():" 
+        #as a condition so that ctrl+C can break the loop
+
+        #publish base velocity using a twist message:
+        #twist_to_motor node converts this into individual wheel velocities 
+        base_velocity.linear.x=0 #velocity in x direction
+        base_velocity.angular.z=0 #velocity around z
+        pub_base_velocity.publish(base_velocity)
 
         rate.sleep()
 
-
+    #may not be needed when using "while not rospy.is_shutdown():"
     # spin() simply keeps python from exiting until this node is stopped
-    rospy.spin()
+    #rospy.spin()
 
 
 
 
 if __name__ == '__main__':
-    #create a unique node
-    rospy.init_node('percentageHandler', anonymous=True)
+    # In ROS, nodes are uniquely named. If two nodes with the same
+    # name are launched, the previous one is kicked off. The
+    # anonymous=True flag means that rospy will choose a unique
+    # name for our 'listener' node so that multiple listeners can
+    # run simultaneously.
+    #create a unique node:
+    rospy.init_node('explore_sentry', anonymous=True)
     #create a publisher object and defines which topic it subscribes to
-    pub = rospy.Publisher('progress', Int32, queue_size=10)
+    #pub = rospy.Publisher('progress', Int32, queue_size=10)
 
-    pub_timer_finished = rospy.Publisher('sentry_control_topic', String, queue_size=10)
+    pub_base_velocity = rospy.Publisher('cmd_vel', Twist, queue_size=10)
+
+    #pub_timer_finished = rospy.Publisher('sentry_control_topic', String, queue_size=10)
     
     #start the subscribing and publishing process
     try:
